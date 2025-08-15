4c12d4dd6ee2d02f3cf1a9ea7c61740a7b81ff09
Hassan Kazi <hassankazinasik@gmail.com>
2025-08-15 16:28:24 +0530

    Background setup

--- React/coders/src/App.jsx
+++ React/coders/src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-
+import Image from "./assets/independence.jpg"
 const App = () => {
   return (
     <div>
-      <h1>Welcome to the independence day website</h1>
+      <img src={Image} className='h-screen w-screen' alt="Independence" style={{ width: '100%', height: 'auto' }} />
+      
     </div>
   )
 }
